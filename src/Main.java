07e322c40a5e76d7d63ee49a12f32052916881dc
SalomeCarter <elizabethboston@icloud.com>
2022-12-28 12:26:40 +0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,47 @@
+// 1. ВЫВЕСТИ МАКС И МИН ЧИСЛА
+//class Main {
+//
+//    public static void main(String args[]){
+//
+//        int[] arr = {-45, 78, 67, 90, 876};
+//
+//        int max = arr[0];
+//        int min = arr[0];
+//        for (int i = 1; i < arr.length; i++) {
+//            max = Math.max(arr[i], max);
+//            min = Math.min(arr[i], min);
+//        }
+//        System.out.println(max);
+//        System.out.println(min);
+//    }
+//}
+
+//2. Вывести чет и не чет числа
+//class Main {
+//
+//    public static void main(String args[]) {
+//        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+//
+//        for (int i = 0; i < array.length; i++)
+//
+//            if (array[i] % 2 == 0) {
+//                System.out.println("Even number: " + array[i]);
+//            } else if (array[i] % 2 == 1) {
+//                System.out.println("Odd number: " + array[i]);
+//            }
+//    }
+//}
+
+//4. Выводить только те числа которые идут в порядке возрастания
+//import java.util.Arrays;
+//
+//public class Main
+//{
+//    public static void main(String[] args)
+//    {
+//        int[] array = {1,4,9,2,4,8,3,6,7};
+//        Arrays.sort(array);
+//        System.out.println(Arrays.toString(array));
+//    }
+//}
+
